9bd118077006f1619c5a904bd590c7cd460959f6
Kononenko Oleg <o.kononenko@qiwi.com>
2025-03-10 15:34:29 +0300

    Add build workflow

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIR="$(pwd)"
+NGINX_VERSION="1.27.3"
+
+echo "Nginx version $NGINX_VERSION"
+echo "Directory: $DIR"
+
+mkdir -p $DIR/buildnginx/modules/ngx_module_vts/
+wget -q "https://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz"
+tar -xzf nginx-${NGINX_VERSION}.tar.gz
+mv nginx-${NGINX_VERSION}/* $DIR/buildnginx/
+mv src  $DIR/buildnginx/modules/ngx_module_vts/
+mv config  $DIR/buildnginx/modules/ngx_module_vts/
+cd $DIR/buildnginx
+./configure --with-cc-opt='-g -O2 -fstack-protector-strong -Wformat -Werror=format-security' --with-pcre --with-stream --with-http_ssl_module --add-module=./modules/ngx_module_vts/
+make -j12
